dcc8a3fcb6855b300de161e5f1494889a86ea29f
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-27 12:15:28 +0700

    Add project files.

---
+++ CarManagement/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarManagement
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarManagement/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CarManagement
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
