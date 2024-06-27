397a64c5fa58082e14809532b08e7e2c0091242f
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-27 16:49:17 +0700

    add car model

---
+++ CarManagement/Model/CarModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CarManagement.Model
+{
+    public class CarModel
+    {
+        public string Name { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string Make { get; set; } = null!;
+        public DateTime Year { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
