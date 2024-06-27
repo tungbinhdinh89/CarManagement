264aa10842ee1535055cb8374069d29dd2743f75
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-27 21:13:19 +0700

    edit car model

--- CarManagement/Model/CarModel.cs
+++ CarManagement/Model/CarModel.cs
@@ -2,11 +2,11 @@
 {
     public class CarModel
     {
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Model { get; set; } = null!;
         public string Make { get; set; } = null!;
-        public DateTime Year { get; set; }
+        public int Year { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime LastUpdatedAt { get; set; }
     }
 }
