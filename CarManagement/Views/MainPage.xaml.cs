9021410f1a53f09f9f265479f98d657996a52904
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-28 10:24:54 +0700

    edit main page

---
+++ CarManagement/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CarManagement.ViewModels;
+
+namespace CarManagement.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+
+}
