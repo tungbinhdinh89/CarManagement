a0bf2cba9753cddaaa280d9562bbed3b5a43b23c
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-28 14:29:20 +0700

    edit add car page to dynamic

--- CarManagement/ViewModels/AddCarViewModel.cs
+++ CarManagement/ViewModels/AddCarViewModel.cs
@@ -3,75 +3,145 @@ using System.Windows.Input;
 
 namespace CarManagement.ViewModels
 {
+    //public class AddCarViewModel : BaseViewModel
+    //{
+    //    private string _pageTitle;
+    //    public string PageTitle
+    //    {
+    //        get => _pageTitle;
+    //        set => SetProperty(ref _pageTitle, value);
+    //    }
+
+    //    private string _buttonText;
+    //    public string ButtonText
+    //    {
+    //        get => _buttonText;
+    //        set => SetProperty(ref _buttonText, value);
+    //    }
+
+    //    private CarModel _car;
+    //    public CarModel Car
+    //    {
+    //        get => _car;
+    //        set => SetProperty(ref _car, value);
+    //    }
+
+    //    public string Name
+    //    {
+    //        get => Car.Name;
+    //        set
+    //        {
+    //            Car.Name = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+    //    public string Model
+    //    {
+    //        get => Car.Model;
+    //        set
+    //        {
+    //            Car.Model = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+    //    public string Make
+    //    {
+    //        get => Car.Make;
+    //        set
+    //        {
+    //            Car.Make = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+    //    public int Year
+    //    {
+    //        get => Car.Year;
+    //        set
+    //        {
+    //            Car.Year = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+    //    public string CreatedAt
+    //    {
+    //        get => Car.CreatedAt;
+    //        set
+    //        {
+    //            Car.CreatedAt = value;
+    //            OnPropertyChanged();
+    //        }
+    //    }
+
+    //    public ICommand SaveCommand { get; }
+    //    public ICommand CancelCommand { get; }
+
+    //    public AddCarViewModel()
+    //    {
+    //        Car = new CarModel();
+    //        SaveCommand = new Command(async () => await SaveCar());
+    //        CancelCommand = new Command(async () => await Cancel());
+    //    }
+
+    //    public AddCarViewModel(CarModel car)
+    //    {
+    //        Car = car;
+    //        PageTitle = "Edit Car";
+    //        ButtonText = "Save";
+    //        SaveCommand = new Command(async () => await SaveCar());
+    //        CancelCommand = new Command(async () => await Cancel());
+    //    }
+
+    //    private async Task SaveCar()
+    //    {
+    //        // Logic to save the car (either add new or update existing)
+    //        // Example: await carService.SaveCarAsync(Car);
+    //    }
+
+    //    private async Task Cancel()
+    //    {
+    //        await Application.Current.MainPage.Navigation.PopAsync();
+    //    }
+    //}
+
     public class AddCarViewModel : BaseViewModel
     {
-        private string name;
-        private string model;
-        private string make;
-        private int year;
-        private string createdAt;
-
-        public string Name
+        private CarModel _car;
+        public CarModel Car
         {
-            get => name;
-            set => SetProperty(ref name, value);
+            get => _car;
+            set => SetProperty(ref _car, value);
         }
 
-        public string Model
-        {
-            get => model;
-            set => SetProperty(ref model, value);
-        }
-
-        public string Make
-        {
-            get => make;
-            set => SetProperty(ref make, value);
-        }
-
-        public int Year
-        {
-            get => year;
-            set => SetProperty(ref year, value);
-        }
-
-        public string CreatedAt
-        {
-            get => createdAt;
-            set => SetProperty(ref createdAt, value);
-        }
-
-        public ICommand AddCarCommand { get; }
+        public string PageTitle { get; set; }
+        public string ActionButtonText { get; set; }
+        public ICommand SaveCarCommand { get; }
         public ICommand CancelCommand { get; }
 
         public AddCarViewModel()
         {
-            AddCarCommand = new Command(async () => await AddCar());
-            CancelCommand = new Command(async () => await Cancel());
+            SaveCarCommand = new Command(SaveCar);
+            CancelCommand = new Command(Cancel);
         }
 
-        private async Task AddCar()
+        private async void SaveCar()
         {
-            // Thêm logic để lưu car mới
-            var newCar = new CarModel();
-            {
-                Name = Name;
-                Model = Model;
-                Make = Make;
-                Year = Year;
-                CreatedAt = CreatedAt;
-            };
-
-            // Add logic to save the new car
-            // For example, call an API to save the car
-
-            // Navigate back to the main page
-            await Shell.Current.GoToAsync("//MainPage");
+            // Logic to save or update the car
         }
 
-        private async Task Cancel()
+        private async void Cancel()
         {
-            await Shell.Current.GoToAsync("//MainPage");
+            await Shell.Current.Navigation.PopAsync();
+        }
+
+        public void Initialize(CarModel car, bool isEditMode)
+        {
+            Car = car;
+            PageTitle = isEditMode ? "Edit Car" : "Add New Car";
+            ActionButtonText = isEditMode ? "Save Changes" : "Add Car";
         }
     }
 }
