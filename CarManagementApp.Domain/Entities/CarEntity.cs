9e43db50c346549f1753dd63343bab6f650b56e9
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-06-29 22:48:20 +0700

    edit car entity

--- CarManagementApp.Domain/Entities/CarEntity.cs
+++ CarManagementApp.Domain/Entities/CarEntity.cs
@@ -3,9 +3,9 @@
     public class CarEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Model { get; set; }
-        public string Make { get; set; }
+        public string Name { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string Make { get; set; } = null!;
         public DateTime Year { get; set; }
         public DateTime CreatedAtAS { get; set; }
 
